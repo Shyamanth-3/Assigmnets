e1b9782bb825748b3347af24ca9bfe64a22cfe67
Shyamanth-3 <shyamanthnadella.03082007@gmail.com>
2025-05-03 16:51:34 +0530

    Assignment-14.2

---
+++ Assignment-14.2/index.js
@@ -0,0 +1,8 @@
+//Increment and Decrement Operators
+let a = 5;
+console.log("Initial value of a: ",a);
+console.log("Pre-increment (++a):", ++a); // Increment first, then use
+console.log("Post-increment (a++):", a++); // Use first, then increment
+console.log("Pre-decrement (--a):", --a); // Decrement first, then use
+console.log("Post-decrement (a--):", a--); // Use first, then decrement
+console.log("Final value of a:", a);
